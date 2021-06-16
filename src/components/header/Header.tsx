653118d3b7c43949d272a278d64e5fe8b823f5c5
RopFoo <ropfoo@gmail.com>
2021-06-16 09:00:50 +0200

    minor header style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { useScrollDirection } from "../../hooks/useScrollDirection"
 
 import owlLogo from "./assets/owl_logo_solo.svg"
 
@@ -8,17 +10,31 @@ import owlLogo from "./assets/owl_logo_solo.svg"
 import "./Header.scss"
 
 const Header = () => {
+  const { isScrollUp } = useScrollDirection()
+
+  const variants = {
+    shown: { y: 0 },
+    hidden: { y: -100 },
+  }
+
   return (
-    <header className="header">
-      <div className="header__logo">
-        <img src={owlLogo} alt="" />
+    <motion.header
+      animate={isScrollUp ? "shown" : "hidden"}
+      variants={variants}
+      transition={{ duration: 0.5, delay: 0.15 }}
+      className="header"
+    >
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <img src={owlLogo} alt="" />
+        </div>
+        <div className="header__nav">
+          <p>me</p>
+          <p>skills</p>
+          <p>projects</p>
+        </div>
       </div>
-      <div className="header__nav">
-        <p>me</p>
-        <p>skills</p>
-        <p>projects</p>
-      </div>
-    </header>
+    </motion.header>
   )
 }
 
